fd74cfb1ff6b88edae389bc1b125edb53628b4b9
Fraser <tomatoesneverlie@gmail.com>
2025-02-06 16:57:00 -0500

    draw residuals

--- analysis.py
+++ analysis.py
@@ -83,18 +83,26 @@ for data in np.array_split(data, num_splits):
 
     observations.append(normalised)
 
+fig, axs = plt.subplots(2)
+# draw black dotted line at 0
+axs[1].axhline(0, color='black', linestyle='--', linewidth=1)
+
+def mean_and_uncertainty(confidence):
+    observed_mean = np.mean(observations, axis=0)
+    observed_mean_uncertainty = np.std(observations, axis=0) / np.sqrt(num_splits)
+    observed_mean_uncertainty *= norm.ppf(0.5 + confidence / 2) - norm.ppf(0.5 - confidence / 2)
+    return observed_mean, observed_mean_uncertainty
+
 # plot the observed data, with error bars derived from the spread per sub-range
 confidences = [0.68, 0.95]
 colours = ['#2d494f', '#5f7c82']
 for i in range(len(confidences) - 1, -1, -1):
     confidence = confidences[i]
-    observed_mean = np.mean(observations, axis=0)
-    observed_mean_uncertainty = np.std(observations, axis=0) / np.sqrt(num_splits)
-    observed_mean_uncertainty *= norm.ppf(0.5 + confidence / 2) - norm.ppf(0.5 - confidence / 2)
+    observed_mean, observed_mean_uncertainty = mean_and_uncertainty(confidence)
 
     bar_width = 2 + 8 * (1 - confidence)
 
-    plt.errorbar(np.arange(num_bins), 
+    axs[0].errorbar(np.arange(num_bins), 
                  observed_mean, 
                  yerr=observed_mean_uncertainty, 
                  elinewidth=bar_width,
@@ -103,23 +111,43 @@ for i in range(len(confidences) - 1, -1, -1):
                  color=colours[i],
                  label=f"Observed Data ({confidence:.0%} confidence)")
 
+residuals_zero, residuals_one = mean_and_uncertainty(0.68)
+
 # fit a poisson distribution to the data, with mean taken from the total data
 poisson_x = np.arange(num_bins)
 poisson_y = poisson.pmf(poisson_x, np.mean(data))
-plt.plot(poisson_x, poisson_y, label="Poisson fit", color='#f2a900')
+params = {'label': "Poisson fit", 'color': '#f2a900', 'linewidth': 1.5}
+axs[0].plot(poisson_x, poisson_y, **params)
+axs[1].plot(poisson_x, (poisson_y - residuals_zero) / residuals_one, marker='o', markersize=3, **params)
 
 # fit a gaussian distribution to the data, with mean and std taken from the total data
 mean, std = np.mean(data), np.std(data)
-gauss_x = np.linspace(0, num_bins, 1000)
+# gauss_x = np.linspace(0, num_bins, 1000)
+gauss_x = np.arange(num_bins)
 gauss_y = norm.pdf(gauss_x, mean, std)
-plt.plot(gauss_x, gauss_y, label="Gaussian fit", color='#de5823')
+params = {'label': "Gaussian fit", 'color': '#de5823', 'linewidth': 1.5}
+axs[0].plot(gauss_x, gauss_y, **params)
+axs[1].plot(gauss_x, (gauss_y - residuals_zero) / residuals_one, marker='o', markersize=3,
+**params)
 
-plt.xlabel("Clicks per 1000ms")
-plt.ylabel("Normalised Probability (0-1)")
+
+axs[1].set_xlabel("Clicks per 1000ms")
+axs[0].set_ylabel("Probability [0-1]")
+axs[1].set_ylabel("Curve Residuals (σ)")
 
 # set x ticks to be integers
-plt.xticks(np.arange(0, num_bins, 1))
+for ax in axs:
+    ax.set_xticks(np.arange(0, num_bins, 1))
+    ax.set_xticklabels(np.arange(0, num_bins, 1))
 
-plt.legend()
+axs[1].set_ylim(-5, 5)
+axs[1].set_yticklabels(map(lambda x: f"{int(x)}σ", axs[1].get_yticks()))
+
+axs[0].legend()
 plt.show()
 
+
+# NOTES FROM Talking with Prof. Ryan
+# - Error bars are probably too big, check that out
+# - Don't normalise data, keep batch size and number of batches in the report
+# - Actually fit both Poisson and Gaussian distributions, give them a fighting chance
