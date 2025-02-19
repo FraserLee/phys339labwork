7795a5095d8ba1808d4e3a5e4db6573bf10ea9ea
Fraser <tomatoesneverlie@gmail.com>
2025-02-19 17:04:42 -0500

    create recorder.py

---
+++ lab4/recorder.py
@@ -0,0 +1,58 @@
+import serial
+import serial.tools.list_ports as lp
+import time
+
+mode = 'transparent'
+# mode = 'polarised'
+
+ports = lp.comports()
+arduino_port = None
+for p in ports:
+    print(p.device, p.description)
+    if 'arduino' in p.description.lower():
+        arduino_port = p.device
+        print('\t⬆️  identified as arduino')
+
+if arduino_port is None:
+    print('No arduino found, exiting')
+    exit()
+
+
+
+
+file_path = f'data/{mode}_{time.strftime("%Y_%m_%d-%H_%M_%S")}.txt'
+file = open(file_path, 'w')
+def log(data):
+    file.write(data.strip() + '\n')
+    file.flush()
+    print('logged [', data.strip(), ']')
+
+buffer = ''
+s = serial.Serial()
+try:
+    s.port = arduino_port
+    s.baudrate = 115200
+    s.open()
+
+    s.write(bytes('100\n', 'utf-8'))
+
+    # read back data
+    while True:
+        while s.in_waiting > 0:
+            piece = s.read()
+            try:
+                buffer += piece.decode('utf-8')
+            except:
+                print('read malformed data:', piece)
+            
+        while '\n' in buffer:
+            idx = buffer.index('\n')
+            packet = ''.join(buffer[:idx+1]).strip()
+            buffer = buffer[idx+1:]
+            log(packet)
+
+except KeyboardInterrupt:
+    file.close()
+    s.close()
+    print('Exiting')
+
